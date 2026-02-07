59136b3dd8a0f240f59eb2c68689518597a95f17
AlexH <ajax.alex@gmail.com>
2026-02-07 10:43:53 -0600

    added shooter PID

--- src/main/java/frc/robot/subsystems/Indexer.java
+++ src/main/java/frc/robot/subsystems/Indexer.java
@@ -4,6 +4,7 @@ import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 
 import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -23,6 +24,12 @@ public class Indexer extends SubsystemBase {
         motor = new SparkMax(motorID, MotorType.kBrushless);
 
         setDefaultCommand(idle());
+    }
+    
+    @Override
+    public void periodic() {
+        // This method will be called once per scheduler run
+        SmartDashboard.putNumber("IndexerVel", motor.getEncoder().getVelocity());
     }
 
     public void run(final double speed) {
